afdda32f15d6ce9c028d4606855c1d1008634269
Dat Pham <duydatyds@gmail.com>
2015-11-02 08:58:01 +0700

    Update web.php

--- config/web.php
+++ config/web.php
@@ -37,9 +37,9 @@ $config = [
             'useFileTransport' => false,
             'transport' => [
                 'class' => 'Swift_SmtpTransport',
-                'host' => 'smtp.gmail.com',  // e.g. smtp.mandrillapp.com or smtp.gmail.com
-                'username' => 'nanayou.dat@gmail.com',
-                'password' => 'duydatyds@gmail.com',
+                'host' => 'localhost',  // e.g. smtp.mandrillapp.com or smtp.gmail.com
+                'username' => 'username',
+                'password' => 'password',
                 'port' => '587', // Port 25 is a very common port too
                 'encryption' => 'tls', // It is often used, check your provider or mail server specs
             ],
